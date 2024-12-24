397b0a8cccdbf77a0f7220e7ee505a1fcded7746
Anavil pandey <151740953+Anavil7723@users.noreply.github.com>
2024-12-24 13:56:28 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("6931904852:AAF5C6656C1JRYyr0awNo5ObY3Fix7TZCHk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@shiva_158")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("700591003")
 
 
 
